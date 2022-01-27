ca74509af909b033d99434cccd8666cba58e213b
matteo2437 <matteo.budriesi@gmail.com>
2022-01-27 09:57:50 +0100

    fix bug

--- Main.java
+++ Main.java
@@ -1,13 +1,27 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.plaf.TableUI;
 
 import components.table.Table;
 
 class Main {
   public static void main(String[] args){
+    try {
+      UIManager.setLookAndFeel(
+        UIManager.getCrossPlatformLookAndFeelClassName());
+    }
+    catch(Exception e) {
+
+    }
     JFrame frame = new JFrame();
 		frame.setTitle("Foglio elettronico");
 
     Table table = new Table(80, 20);
+
+
 
 		frame.add(table);
 		frame.setSize(720, 480);
